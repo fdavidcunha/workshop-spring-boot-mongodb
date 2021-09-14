197fe226d42c0cd44e9125f35ab2b53149d4b521
David <f.david.cunha@gmail.com>
2021-09-14 07:31:30 -0300

    Entity User Rest

---
+++ src/main/java/com/davidferreira/workshopmongo/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.davidferreira.workshopmongo.resources;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.davidferreira.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		return ResponseEntity.ok(null);
+	}
+	
+}
